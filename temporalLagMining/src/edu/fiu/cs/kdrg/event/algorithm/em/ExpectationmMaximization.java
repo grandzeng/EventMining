02f65c16af031ce4388ff5dbfbe3a2f1b80b9e3c
grandzeng <grandzeng@gmail.com>
2014-02-05 11:09:25 -0500

    add abstract class for EM

---
+++ temporalLagMining/src/edu/fiu/cs/kdrg/event/algorithm/em/ExpectationmMaximization.java
@@ -0,0 +1,72 @@
+/**
+ * 
+ */
+package edu.fiu.cs.kdrg.event.algorithm.em;
+
+/**
+ * This abstract class gives the skeleton of expectation-maximization algorithm
+ * 
+ * @author Chunqiu Zeng
+ * @date Feb. 5th, 2014
+ */
+
+public abstract class ExpectationmMaximization {
+
+	/**
+	 * The objective is to maximize the logLikelihood
+	 */
+	protected double logLikelihood = Double.NEGATIVE_INFINITY;
+
+	/**
+	 * the # of iteration
+	 */
+	protected int iteration = 0;
+
+	/**
+	 * The stopping criteria
+	 */
+	protected double threshold = 0.0;
+
+	/**
+	 * This function is used to log the information for monitoring the algorithm
+	 */
+	public abstract void logInfo();
+
+	/**
+	 * initialization
+	 */
+	public abstract void initialization();
+
+	/**
+	 * calculate the posterior probability
+	 */
+	public abstract void expectation();
+
+	/**
+	 * calculate the parameters for maximization
+	 */
+	public abstract void maximization();
+
+	/**
+	 * Based on the new updated parameters, the new loglikelihood is evaluated
+	 */
+	public abstract void evaluation();
+
+	/**
+	 * The entry for running Expectation-Maximization algorithm
+	 */
+	public void run() {
+		double oldLogLikelihood = logLikelihood;
+		double delta = 0.0;
+		initialization();
+		do {
+			expectation();
+			maximization();
+			evaluation();
+			delta = Math.abs(logLikelihood - oldLogLikelihood);
+			iteration++;
+			logInfo();
+		} while (delta <= threshold);
+	}
+
+}
