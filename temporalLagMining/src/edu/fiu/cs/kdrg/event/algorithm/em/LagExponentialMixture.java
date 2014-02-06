99b802165a590f93f80ff723d6d092b13e8a89ce
grandzeng <grandzeng@gmail.com>
2014-02-06 15:30:41 -0500

    add expectation

--- temporalLagMining/src/edu/fiu/cs/kdrg/event/algorithm/em/LagExponentialMixture.java
+++ temporalLagMining/src/edu/fiu/cs/kdrg/event/algorithm/em/LagExponentialMixture.java
@@ -3,55 +3,184 @@
  */
 package edu.fiu.cs.kdrg.event.algorithm.em;
 
+import edu.fiu.cs.kdrg.evevt.util.DensityEval;
+
 /**
  * @author Chunqiu Zeng
- *
+ * 
  */
 public class LagExponentialMixture extends AbstractExpectationmMaximization {
 
-	/* (non-Javadoc)
-	 * @see edu.fiu.cs.kdrg.event.algorithm.em.AbstractExpectationmMaximization#logInfo()
+	/**
+	 * a sequence of time stamps of event A's occurrence
+	 */
+	private double[] antecedents = null;
+
+	/**
+	 * a sequence of time stamps of event B's occurrence
+	 */
+	private double[] consequents = null;
+
+	/**
+	 * The priori probability that an event B is implied by some event A
+	 */
+	private double[] PIs = null;
+
+	/**
+	 * the mean of normal distribution
+	 */
+	private double lambda = 10.0;
+
+	/**
+	 * this is used to record the intermediate result
+	 */
+	private double intermediate[][] = null;
+
+	/**
+	 * default constructor
+	 */
+	public LagExponentialMixture() {
+	}
+
+	/**
+	 * constructor
+	 * 
+	 * @param antecedents
+	 * @param consequents
+	 * @param lambda
+	 */
+	public LagExponentialMixture(double antecedents[], double[] consequents,
+			double lambda) {
+		assert (antecedents != null);
+		assert (consequents != null);
+		this.antecedents = antecedents;
+		this.consequents = consequents;
+		this.lambda = lambda;
+		PIs = new double[antecedents.length];
+
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * edu.fiu.cs.kdrg.event.algorithm.em.AbstractExpectationmMaximization#logInfo
+	 * ()
 	 */
 	@Override
 	public void logInfo() {
 		// TODO Auto-generated method stub
-
+		StringBuffer sb = new StringBuffer();
+		sb.append("(#iteration=");
+		sb.append(iteration);
+		sb.append("): lambda=");
+		sb.append(lambda);
+		sb.append(",logLikelihood=");
+		sb.append(logLikelihood);
+		System.out.println(sb.toString());
 	}
 
-	/* (non-Javadoc)
-	 * @see edu.fiu.cs.kdrg.event.algorithm.em.AbstractExpectationmMaximization#initialization()
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see edu.fiu.cs.kdrg.event.algorithm.em.AbstractExpectationmMaximization#
+	 * initialization()
 	 */
 	@Override
 	public void initialization() {
 		// TODO Auto-generated method stub
+		double average = 1.0 / PIs.length;
+		for (int i = 0; i < PIs.length; i++) {
+			PIs[i] = average;
+		}
 
+		intermediate = new double[consequents.length][antecedents.length];
 	}
 
-	/* (non-Javadoc)
-	 * @see edu.fiu.cs.kdrg.event.algorithm.em.AbstractExpectationmMaximization#expectation()
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see edu.fiu.cs.kdrg.event.algorithm.em.AbstractExpectationmMaximization#
+	 * expectation()
 	 */
 	@Override
 	public void expectation() {
 		// TODO Auto-generated method stub
+		double temp = 0.0;
 
+		for (int i = 0; i < consequents.length; i++) {
+			temp = 0.0;
+			for (int j = 0; j < antecedents.length; j++) {
+				intermediate[i][j] = PIs[j]
+						* DensityEval.exponentialDistribution(lambda,
+								consequents[i] - antecedents[j]);
+				temp += intermediate[i][j];
+			}
+			if (temp != 0)
+				for (int j = 0; j < antecedents.length; j++) {
+					intermediate[i][j] = intermediate[i][j] / temp;
+				}
+			else
+				for (int j = 0; j < antecedents.length; j++) {
+					intermediate[i][j] = PIs[j];
+				}
+			// intermediate[i][antecedents.length] = 1.0;
+		}
 	}
 
-	/* (non-Javadoc)
-	 * @see edu.fiu.cs.kdrg.event.algorithm.em.AbstractExpectationmMaximization#maximization()
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see edu.fiu.cs.kdrg.event.algorithm.em.AbstractExpectationmMaximization#
+	 * maximization()
 	 */
 	@Override
 	public void maximization() {
 		// TODO Auto-generated method stub
+		double newLambda = 0.0;
+		double temp = 0.0;
 
+		// maximize the log likelihood with respect to lambda
+		for (int i = 0; i < consequents.length; i++) {
+			for (int j = 0; j < antecedents.length; j++) {
+				newLambda += (intermediate[i][j] * (consequents[i] - antecedents[j]));
+			}
+		}
+		newLambda = newLambda / consequents.length;
+		lambda = newLambda;
+
+		// maximize the log likelihood with respect to PI
+		for (int j = 0; j < antecedents.length; j++) {
+			temp = 0.0;
+			for (int i = 0; i < consequents.length; i++) {
+				temp += intermediate[i][j];
+			}
+			temp = temp / consequents.length;
+			PIs[j] = temp;
+		}
 	}
 
-	/* (non-Javadoc)
-	 * @see edu.fiu.cs.kdrg.event.algorithm.em.AbstractExpectationmMaximization#evaluation()
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see edu.fiu.cs.kdrg.event.algorithm.em.AbstractExpectationmMaximization#
+	 * evaluation()
 	 */
 	@Override
 	public void evaluation() {
 		// TODO Auto-generated method stub
+		double newLogLikelihood = 0.0;
 
+		for (int i = 0; i < consequents.length; i++) {
+			double sum = 0.0;
+			for (int j = 0; j < antecedents.length; j++) {
+				sum += (intermediate[i][j] * DensityEval
+						.exponentialDistribution(lambda, consequents[i]
+								- antecedents[j]));
+			}
+			newLogLikelihood += Math.log(sum);
+		}
+		logLikelihood = newLogLikelihood;
 	}
 
 }
---
+++ temporalLagMining/test/edu/fiu/cs/kdrg/evevt/algorithm/em/TestRevisedExponentialMixture.java
@@ -0,0 +1,79 @@
+/**
+ * 
+ */
+package edu.fiu.cs.kdrg.evevt.algorithm.em;
+
+import static org.junit.Assert.*;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import edu.fiu.cs.kdrg.event.algorithm.em.LagGaussianMixture;
+import edu.fiu.cs.kdrg.event.algorithm.em.RevisedLagExponentialMixture;
+import edu.fiu.cs.kdrg.evevt.util.DEventLoader;
+
+/**
+ * @author grand
+ * 
+ */
+public class TestRevisedExponentialMixture {
+
+	private RevisedLagExponentialMixture rlem = null;
+
+	private String fileName = null;
+
+	private double lambda = 1.0;
+
+	public void loadParameter() throws IOException {
+		BufferedReader br = new BufferedReader(new FileReader("param/expparam"));
+		String line = null;
+
+		while ((line = br.readLine()) != null) {
+			line = line.trim();
+			if (line.equals(""))
+				continue;
+			String[] pats = line.split("\\s+");
+			fileName = pats[0];
+			lambda = Double.parseDouble(pats[1]);
+			break;
+		}
+
+		br.close();
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+		loadParameter();
+		DEventLoader<String> loader = new DEventLoader<String>();
+		loader.load(fileName);
+		double[] A = loader.getEvent(0);
+		double[] B = loader.getEvent(1);
+		rlem = new RevisedLagExponentialMixture(A, B, lambda);
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	/**
+	 * Test method for
+	 * {@link edu.fiu.cs.kdrg.event.algorithm.em.AbstractExpectationmMaximization#run()}
+	 * .
+	 */
+	@Test
+	public void testRun() {
+		rlem.run();
+	}
+
+}
